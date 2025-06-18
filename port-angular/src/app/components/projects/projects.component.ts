e0e9249c077889b0fc30cf3cf513fc8e06e9a26f
nuttapon <aladen0009@gmail.com>
2025-06-18 23:52:04 +0700

    projects section ui polish

--- port-angular/src/app/components/projects/projects.component.ts
+++ port-angular/src/app/components/projects/projects.component.ts
@@ -2,11 +2,11 @@ import { enterAnimation } from './../../animation/animation';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-projects',
-    templateUrl: './projects.component.html',
-    styleUrls: ['./projects.component.scss'],
-    animations: [enterAnimation],
-    standalone: false
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss'],
+  animations: [enterAnimation],
+  standalone: false
 })
 export class ProjectsComponent implements OnInit {
   projects = [
@@ -14,19 +14,19 @@ export class ProjectsComponent implements OnInit {
       id: 0,
       title: 'Parcel Management',
       description:
-        'This project was created by me. After my dad told me to do it for his job',
+        'Personal project, Implemented with Java and Java Swing library',
       link: 'https://github.com/nuttaponkhamkul123/parcel1.0',
       img: './assets/images/projects/proj1.png',
     },
     {
       id: 1,
       title: 'CS251 Final Project',
-      description: 'Computer part E-commerce using PHP and mySQL ',
+      description: 'Computer part E-commerce using PHP and mySQL for learning database design ',
       link: 'https://github.com/nuttaponkhamkul123/bij',
       img: './assets/images/projects/proj2.png',
     },
   ];
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
