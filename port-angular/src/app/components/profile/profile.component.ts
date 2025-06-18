bfb2d5ecc93089bf16ab79600ba9b428bcf7bb6a
nuttapon <aladen0009@gmail.com>
2025-06-18 23:31:30 +0700

    make profile component same as resume

--- port-angular/src/app/components/profile/profile.component.ts
+++ port-angular/src/app/components/profile/profile.component.ts
@@ -1,13 +1,18 @@
 import { enterAnimation } from 'src/app/animation/animation';
 import { Component, OnInit } from '@angular/core';
+import { SkillsComponent } from './skills/skills.component';
+import { CommonModule } from '@angular/common';
+
+
 
 
 @Component({
-    selector: 'app-profile',
-    templateUrl: './profile.component.html',
-    styleUrls: ['./profile.component.scss'],
-    animations: [enterAnimation],
-    standalone: false
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
+  animations: [enterAnimation],
+  imports: [CommonModule, SkillsComponent],
+  standalone: true
 })
 export class ProfileComponent implements OnInit {
 
