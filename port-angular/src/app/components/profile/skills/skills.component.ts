cfa59837273747cee8f0f0476ead460add277e02
nuttapon <aladen0009@gmail.com>
2025-06-18 23:31:47 +0700

    removed skill bar and add more skills

--- port-angular/src/app/components/profile/skills/skills.component.ts
+++ port-angular/src/app/components/profile/skills/skills.component.ts
@@ -1,39 +1,75 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-skills',
-    templateUrl: './skills.component.html',
-    styleUrls: ['./skills.component.scss'],
-    standalone: false
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss'],
+  imports: [CommonModule],
+  standalone: true
 })
 export class SkillsComponent implements OnInit {
   prog_skill = [
     {
-      title : 'Javascript',
-      power : 30
+      title: 'Javascript',
+      power: 30
     },
     {
-      title : 'Java',
-      power : 20
+      title: 'Java',
+      power: 20
     },
     {
-      title : 'Angular',
-      power : 30
-    }
+      title: 'Angular',
+      power: 30
+    },
+    {
+      title: 'NodeJS',
+      power: 30
+    },
+    {
+      title: 'React',
+      power: 30
+    },
+    {
+      title: 'C#',
+      power: 30
+    },
+    {
+      title: 'Typescript',
+      power: 30
+    },
+    {
+      title: 'RxJS',
+      power: 10
+    },
+    {
+      title: 'Docker',
+      power: 10
+    },
+    {
+      title: 'TailwindCSS',
+      power: 10
+    },
+    {
+      title: 'Joi',
+      power: 10
+    },
   ]
   other_skill = [
     {
-      title : 'Microsoft Word',
-      power : 25
+      title: 'Microsoft Word',
+      power: 25
     },
     {
-      title : 'Microsoft PowerPoint',
-      power : 10
+      title: 'Microsoft PowerPoint',
+      power: 10
     },
     {
-      title : 'Microsoft Excel',
-      power : 10
-    }
+      title: 'Microsoft Excel',
+      power: 10
+    },
+
+
   ]
   constructor() { }
 
